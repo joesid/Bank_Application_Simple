ada5c988be47ce09d875f369ec8335438eae2bed
Joe <joesidious@gmail.com>
2023-04-12 23:40:24 +0100

    1st Push

---
+++ Bank_Application_Simple/Bank_Application_Simple/Bank_Application_Simple.cpp
@@ -0,0 +1,12 @@
+﻿// Bank_Application_Simple.cpp : Defines the entry point for the application.
+//
+
+#include "Bank_Application_Simple.h"
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
---
+++ Bank_Application_Simple/Bank_Application_Simple/Bank_Application_Simple.h
@@ -0,0 +1,49 @@
+﻿// Bank_Application_Simple.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+// TODO: Reference additional headers your program requires here.
+
+class Account
+{
+
+public:
+	int acct_no;
+	string acct_name;
+	double balance;
+
+
+	void Deposit(int acct_no, string acct_name, double amount);
+	void Withdraw(int acct_no, string acct_name, double amount);
+	void Display(int acct_no, string acct_name, double amount);
+};
+
+void Account:: Deposit(int acct_no, string acct_name, double amount) {
+
+	balance += amount;
+}
+
+void Account::Withdraw(int acct_no, string acct_name, double amount)
+{
+	balance -= amount;
+}
+
+void Account::Display(int acct_no, string acct_name, double amount){
+}
+
+//Account Class Ends
+
+
+
+class Savings :Account {
+
+};
+
+class Checking : Account {
+
+
+};
